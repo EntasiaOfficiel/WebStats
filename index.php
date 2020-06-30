8e78f8daa8aababea4253f394be171f77cc5d36d
iTrooz <itrooz.entasia@gmail.com>
2020-06-30 12:42:48 +0200

    a

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>Rice Consumption</title>
+		<script src='chart.min.js'></script>
+		<script src='script.js'></script>
+		<link rel="stylesheet" type="text/css" href="other/global.css">
+	</head>
+	<body>
+		<div id="menu">
+			<?php
+				for
+			?>
+		</div>
+		<p id="separator"> </p>
+		<div id="content">
+			<canvas id="data" width="600" height="400"></canvas>
+		</div>
+	</body>
+</html>
---
+++ script.js
@@ -0,0 +1,63 @@
+window.onload = function(){
+    var rice = document.getElementById('rice').getContext('2d');
+    new Chart(rice, {
+        type: 'line',
+        data: data,
+        options: {
+            scales: {
+                xAxes: [{
+                    display: true,
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Temps en heure'
+                    }
+                }],
+
+                yAxes: [{
+                    ticks:{
+                        min: 0,
+                        max: 8,
+                        stepSize: 0.5
+                    },
+                    display: true,
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'RAM'
+                    }
+                }]
+            }
+        }
+    })
+}
+var data = {
+    labels: [],
+    datasets : [{
+        label : 'test',
+        fillColor : "rgba(172,194,132,0.4)",
+        strokeColor : "#ACC26D",
+        pointColor : "#fff",
+        pointStrokeColor : "#9DB86D",
+        data: [{
+            x: 10,
+            y: 5
+        },
+        {
+            x: 15,
+            y: 2
+        }]
+    }]
+}
+for(let i=0;i<=24;i++)data.labels.push(i)
+
+
+var xmlhttp = new XMLHttpRequest()
+var url = "http://localhost/code/test.txt"
+
+xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        console.log(this.responseText)
+    }
+}
+
+xmlhttp.open("GET", url, true)
+xmlhttp.send()
